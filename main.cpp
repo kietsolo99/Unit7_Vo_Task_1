82ff1d47dc540ec8d9a0460f3de79741d4a4976c
kietsolo99 <kiet.vo054@my.tccd.edu>
2020-02-25 01:35:42 +0000

    Working on structs with C++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-// Unit7_Kiet_Vo 2-24-2020
+// Unit7_Kiet_Vo 2-24-2020 STRUCTS C++
 #include <iostream>
+using namespace std;
+//declare structs above int main()
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  
+  return 0;
 }
